8319a0a1a6d5b1e597116b1dfcb0d8f477c20bd2
alexandrav01 <93940491+alexandrav01@users.noreply.github.com>
2023-05-12 16:48:44 +0100

    Filter integers in an array

---
+++ 1listFilter.js
@@ -0,0 +1,18 @@
+// Create a function that takes a list of non-negative 
+// integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+   let list = []
+   for(let i=0; i<l.length; i++){
+    if (Number.isInteger(l[i])){
+        list.push(l[i])
+    }
+   }
+   return list
+  }
+  console.log(filter_list([1,2,'a','b']))
