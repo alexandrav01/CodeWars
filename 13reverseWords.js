d96b5beca4730cf2bec52b8f47b5215dd53d1f51
alexandrav01 <93940491+alexandrav01@users.noreply.github.com>
2023-05-20 09:35:21 +0100

    Update 13reverseWords.js

--- 13reverseWords.js
+++ 13reverseWords.js
@@ -2,11 +2,13 @@
 function reverseWords(str) {
   let splitString = str.split("")
   console.log("Split String is ", splitString)
+
   let reverseArray = splitString.reverse()
   console.log("Reversed Array is ", reverseArray)
+
   var newString = reverseArray.join("")
   console.log("New String is ", newString)
 
   return newString
   }
-  console.log(reverseWords("This is awesome"))
+  console.log(reverseWords("awesome food"))
